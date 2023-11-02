5c2fc9b3b678b46b07093bbf1fa71f3e7769b871
Hilkka Virtanen <hvirtane@local>
2023-11-02 22:52:18 +0200

    Viikko 2: 3. Pylint ja koodin staattinen analyysi

--- src/index.py
+++ src/index.py
@@ -1,10 +1,6 @@
 from varasto import Varasto
 
-
-def main():
-    mehua = Varasto(100.0)
-    olutta = Varasto(100.0, 20.2)
-
+def osa1(mehua, olutta):
     print("Luonnin jälkeen:")
     print(f"Mehuvarasto: {mehua}")
     print(f"Olutvarasto: {olutta}")
@@ -22,9 +18,9 @@ def main():
     mehua.ota_varastosta(3.14)
     print(f"Mehuvarasto: {mehua}")
 
+def osa2(mehua, olutta, huono):
     print("Virhetilanteita:")
     print("Varasto(-100.0);")
-    huono = Varasto(-100.0)
     print(huono)
 
     print("Varasto(100.0, -50.7)")
@@ -41,6 +37,7 @@ def main():
     mehua.lisaa_varastoon(-666.0)
     print(f"Mehuvarasto: {mehua}")
 
+def osa3(mehua, olutta):
     print(f"Olutvarasto: {olutta}")
     print("olutta.ota_varastosta(1000.0)")
     saatiin = olutta.ota_varastosta(1000.0)
@@ -52,6 +49,16 @@ def main():
     saatiin = mehua.ota_varastosta(-32.9)
     print(f"saatiin {saatiin}")
     print(f"Mehuvarasto: {mehua}")
+
+
+def main():
+    mehua = Varasto(100.0)
+    olutta = Varasto(100.0, 20.2)
+    huono = Varasto(-100.0)
+
+    osa1(mehua, olutta)
+    osa2(mehua, olutta, huono)
+    osa3(mehua, olutta)
 
 
 if __name__ == "__main__":
